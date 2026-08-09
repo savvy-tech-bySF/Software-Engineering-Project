6e71654da1934cef45abba3c38d645a8320451f9
Sajal Fatima <sf08108@st.habib.edu.pk>
2026-08-09 17:21:38 +0500

    deploy

--- SE Project/app.py
+++ SE Project/app.py
@@ -271,5 +271,5 @@ def static_files(filename):
     return send_from_directory('.', filename)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
